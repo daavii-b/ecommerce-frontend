78d4fd5ba944b3654d517678a8a4ac1f611fad63
daavii-b <britodavi122@gmail.com>
2022-12-18 18:19:16 -0300

    added a responsive navbar to mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,13 +4,13 @@ import { useSelector } from 'react-redux';
 
 import React from 'react';
 import {
-  FaHome,
   FaSignOutAlt,
   FaSignInAlt,
   FaShoppingCart,
   FaUser,
   FaSearch,
   FaChevronRight,
+  FaBars,
 } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
@@ -20,23 +20,42 @@ import { Header, Nav, Form } from './styled';
 export default function MainHeader() {
   const { isAuthenticated } = useSelector((state) => state.authReducer);
 
-  const navHandleClick = () => {
-    // if (event.type === 'touchstart') event.preventDefault();
-    // const toggleMenu = document.querySelectorAll('.toggle-menu');
+  const categoryNavHandleClick = () => {
+    const toggleMenu = document.querySelector('.toggle-category-menu');
     const categoryNav = document.querySelector('.category-nav');
 
     categoryNav.classList.toggle('active');
+
+    if (categoryNav.classList.contains('active')) {
+      toggleMenu.setAttribute('aria-expanded', true);
+    } else {
+      toggleMenu.setAttribute('aria-expanded', false);
+    }
+  };
+
+  const mainNavHandleClick = () => {
+    const toggleNavBar = document.querySelector('.toggle-navbar');
+    const mainNavbar = document.querySelector('.main-navbar');
+
+    mainNavbar.classList.toggle('active');
+
+    toggleNavBar.setAttribute('aria-expanded', true);
+
+    setTimeout(() => {
+      mainNavbar.classList.remove('active');
+      toggleNavBar.setAttribute('aria-expanded', false);
+    }, 3000);
   };
 
   return (
     <Header>
       <nav className="category-nav">
-        <button type="button" className="toggle-menu" aria-expanded="false">
-          <FaChevronRight
-            size={10}
-            // onTouchStart={menuHandleClick}
-            onClick={navHandleClick}
-          />
+        <button
+          type="button"
+          className="toggle-category-menu"
+          aria-expanded="false"
+        >
+          <FaChevronRight size={10} onClick={categoryNavHandleClick} />
         </button>
         <ul className="category-list">
           <li className="category-item">
@@ -54,15 +73,17 @@ export default function MainHeader() {
         </ul>
         <button
           type="button"
-          onClick={navHandleClick}
-          className="toggle-menu off"
+          onClick={categoryNavHandleClick}
+          className="toggle-category-menu off"
         >
           <AiOutlineClose size={10} />
         </button>
       </nav>
 
       <div>
-        <h1>E-commerce</h1>
+        <h1>
+          <Link to="/">E-commerce</Link>
+        </h1>
       </div>
 
       <Form className="search-form">
@@ -73,32 +94,37 @@ export default function MainHeader() {
             name="search"
             id="search"
             className="search-input"
+            placeholder="Search for products"
           />
         </label>
       </Form>
 
-      <Nav>
-        <ul>
-          <li>
-            <Link to="/">
-              <FaHome size={18} className="icons" />
-            </Link>
-          </li>
+      <Nav className="main-navbar">
+        <button
+          type="button"
+          className="toggle-navbar"
+          aria-expanded="false"
+          onClick={mainNavHandleClick}
+        >
+          <FaBars size={18} />
+        </button>
+
+        <ul className="nav-items">
           <li>
             <Link to="#">
-              <FaShoppingCart size={18} className="icons" />
+              <FaShoppingCart size={20} className="icons" />
             </Link>
           </li>
           {isAuthenticated ? (
             <li>
               <Link to="/user">
-                <FaUser size={18} className="icons" />
+                <FaUser size={20} className="icons" />
               </Link>
             </li>
           ) : (
             <li>
               <Link to="/login">
-                <FaSignInAlt size={18} className="icons" />
+                <FaSignInAlt size={20} className="icons" />
               </Link>
             </li>
           )}
@@ -106,7 +132,7 @@ export default function MainHeader() {
           {isAuthenticated ? (
             <li>
               <Link to="#">
-                <FaSignOutAlt size={18} className="icons" />
+                <FaSignOutAlt size={20} className="icons" />
               </Link>
             </li>
           ) : (
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -4,12 +4,12 @@ import * as defaultStyles from '../../styles/defaultStyles';
 import { device } from '../../styles/mediaQueries';
 
 export const Header = styled.header`
-  z-index: 2;
   position: relative;
   height: 8rem;
 
   display: grid;
-  grid-template-columns: 23rem 1fr 20%;
+  grid-template-columns: 18rem 1fr 20%;
+  gap: 0.4rem;
   align-items: center;
   justify-items: center;
 
@@ -29,24 +29,29 @@ export const Header = styled.header`
     top: 8rem;
     left: 0;
 
-    .toggle-menu {
-      border: none;
-      border-radius: 0 0 0.4rem 0.4rem / 0 0.6rem 0.6rem;
+    z-index: 100;
+
+    .toggle-category-menu {
+      display: block;
+
       background: transparent;
 
-      width: 4rem;
+      border: none;
+      border-radius: 0 0 0.4rem 0.4rem / 0 0.6rem 0.6rem;
+
+      width: 2.5rem;
       height: 4rem;
       padding: 0.2rem;
 
-      display: block;
       position: absolute;
       top: 0;
       left: 0;
+
       transition: 0.6s ease-out;
 
       svg {
-        width: 70%;
-        height: 70%;
+        width: 50%;
+        height: 50%;
       }
 
       visibility: visible;
@@ -60,8 +65,6 @@ export const Header = styled.header`
       height: 70vh;
       overflow: hidden;
 
-      display: flex;
-      flex-direction: column;
       padding: 0.5rem;
 
       border-top: none;
@@ -79,6 +82,7 @@ export const Header = styled.header`
         a {
           display: inline-block;
           padding: 0.5rem;
+
           font-size: 1.2rem;
           text-transform: capitalize;
 
@@ -98,10 +102,15 @@ export const Header = styled.header`
       }
     }
 
-    .toggle-menu.off {
+    .toggle-category-menu.off {
       visibility: hidden;
       transition: 0.6s ease-out;
       opacity: 0;
+
+      svg {
+        width: 70%;
+        height: 70%;
+      }
     }
   }
 
@@ -115,61 +124,54 @@ export const Header = styled.header`
     transition: 0.6s ease-in;
   }
 
-  .category-nav.active .toggle-menu {
+  .category-nav.active .toggle-category-menu {
     left: 15rem;
+
     transition: 0.6s ease-in;
+
     visibility: hidden;
     opacity: 0;
   }
 
-  .category-nav.active .toggle-menu.off {
+  .category-nav.active .toggle-category-menu.off {
     left: 15rem;
+
     visibility: visible;
-    transition: 0.6s ease-in;
     opacity: 1;
-  }
 
-  .ecommerce-image {
-    width: 4rem;
-    height: 4rem;
-    border: 1px solid rgba(0, 0, 0, 0.03);
-    box-shadow: 0 1px 5px 2px rgba(0, 0, 0, 0.06);
-    border-radius: 50%;
-    overflow: hidden;
-
-    img {
-      width: 100%;
-      height: 100%;
-    }
+    transition: 0.6s ease-in;
   }
 
   div {
     display: flex;
     align-items: center;
+    justify-content: center;
 
     h1 {
-      font-size: 2.4rem;
+      font-size: 1.8rem;
       font-style: italic;
+
       text-shadow: 0 3px 0.5px rgba(255, 255, 255, 0.2);
+
       letter-spacing: 5px;
     }
   }
 
-  @media ${device.tablet} {
+  @media ${device.tabletL} {
     div {
       h1 {
-        font-size: 1.8rem;
+        font-size: 1.5rem;
       }
     }
-    grid-template-columns: 22rem 1fr;
+    grid-template-columns: 16rem 1fr;
   }
 
-  @media ${device.mobileM} {
+  @media ${device.mobileL} {
     grid-template-columns: 1fr;
 
     div {
       h1 {
-        font-size: 1.6rem;
+        font-size: 1.3rem;
         letter-spacing: 4px;
       }
     }
@@ -178,13 +180,49 @@ export const Header = styled.header`
 
 export const Nav = styled.nav`
   width: 100%;
-  margin: 0 auto;
+  z-index: 100;
 
-  ul {
+  position: relative;
+  top: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  transition: 0.3s ease-in;
+
+  button.toggle-navbar {
+    position: absolute;
+    top: 8rem;
+    right: 0;
+
+    background-color: transparent;
+    border: none;
+
+    padding: 0.4rem;
+
+    transition: all 0.4 ease-in;
+
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  ul.nav-items {
+    position: relative;
+    top: 0rem;
+    right: 0;
+
+    width: 100%;
+
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
+
+    visibility: visible;
+    opacity: 1;
 
     li {
       margin: 0 0.2rem;
@@ -195,15 +233,110 @@ export const Nav = styled.nav`
       }
     }
   }
+
+  @media ${device.tabletL} {
+    width: fit-content;
+
+    position: absolute;
+    top: 8rem;
+    right: 0;
+    transition: all 0.4 ease-in;
+
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-end;
+
+    button.toggle-navbar {
+      width: 4rem;
+      height: 4rem;
+
+      position: relative;
+      top: 0;
+      right: 0;
+      display: block;
+
+      background-color: transparent;
+
+      padding: 0.4rem;
+
+      transition: all 0.5s ease-out;
+
+      opacity: 1;
+      visibility: visible;
+    }
+
+    ul.nav-items {
+      width: 0px;
+
+      opacity: 0;
+      visibility: hidden;
+
+      overflow: hidden;
+      position: absolute;
+      top: 0;
+      right: 0;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+
+      transition: 0.5s ease-in;
+
+      li {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        margin: 0.3rem 0;
+
+        width: 4rem;
+        height: 4rem;
+
+        background-color: ${colors.productColor};
+
+        border-radius: 50%;
+        box-shadow: 0 3px 10px 3px rgba(0, 0, 0, 0.1);
+
+        a svg {
+          color: rgba(20, 20, 20, 0.8);
+        }
+      }
+    }
+
+    &.active {
+      button.toggle-navbar {
+        position: absolute;
+        padding: 0.4rem;
+
+        transition: all 0.5s ease-in;
+
+        opacity: 0;
+        visibility: hidden;
+      }
+
+      ul.nav-items {
+        width: 5rem;
+        height: fit-content;
+        position: relative;
+
+        transition: 0.5s ease-out;
+
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+  }
 `;
 
 export const Form = styled.form`
   width: 100%;
+
   display: flex;
   align-items: center;
 
   label {
     width: 100%;
+
     display: flex;
     align-items: center;
     justify-content: center;
@@ -214,14 +347,18 @@ export const Form = styled.form`
     }
   }
   .search-input {
-    margin-left: 1rem;
-    padding: 0.3rem 0.5rem;
-    border-radius: 0.6rem;
-    width: 100%;
+    width: 95%;
     max-width: 100%;
+
+    margin-left: 1rem;
+    padding: 0.3rem 0.6rem;
+
+    border-radius: 0.6rem;
     border: 1px solid rgba(0, 0, 0, 0.2);
-    transition: all 0.2s ease-in-out;
+
     box-shadow: 0 0 4px 3px rgba(0, 0, 0, 0.1);
+
+    transition: all 0.2s ease-in-out;
 
     &:focus {
       transition: all 0.2s ease-in;
